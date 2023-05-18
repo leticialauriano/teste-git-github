a8fa48d9a5755396c3b6d13ef3743cd2747fc6a5
Letícia Lauriano <leticia_lauriano@hotmail.com>
2023-05-18 12:46:14 -0300

    Adicionando Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Bem vindo ao teste de Git e Github da Letícia");
+
+Console.WriteLine("Inserindo mensagem teste");
